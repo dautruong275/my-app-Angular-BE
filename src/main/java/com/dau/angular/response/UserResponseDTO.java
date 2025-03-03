2c179585d6d6712be0275958985de894d982f7ac
dautruong275 <dautruong.dt@gmail.com>
2025-03-03 08:33:45 +0700

    no message

--- src/main/java/com/dau/angular/response/UserResponseDTO.java
+++ src/main/java/com/dau/angular/response/UserResponseDTO.java
@@ -1,12 +1,12 @@
 package com.dau.angular.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDTO {
     private Long id;
     private String username;
